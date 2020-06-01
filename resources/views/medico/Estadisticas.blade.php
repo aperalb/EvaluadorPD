8fedccfd361056c03ce8b4a517714fd2c5a8d9d7
Chinaski <aperalb@gmail.com>
2020-06-01 09:51:59 +0200

    Ultimas modificaciones en dashboard

--- resources/views/medico/Estadisticas.blade.php
+++ resources/views/medico/Estadisticas.blade.php
@@ -21,11 +21,11 @@
            </tr>
            <tr>
                <td>Media de edad</td>
-               <td>{{$mediaEdad}}</td>
+               <td>{{round($mediaEdad,2)." años"}}</td>
            </tr>
            <tr>
                <td>Media de edad en origen de enfermedad</td>
-               <td>{{$mediaEdadInitPD}}</td>
+               <td>{{round($mediaEdadInitPD,2)." años"}}</td>
            </tr>
            <tr>
                <td>Número de evaluaciones registradas en el sistema</td>
